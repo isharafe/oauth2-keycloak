3a25679e5e7ac70118737887578d59c38a72253b
Ishara <isharafe@gmail.com>
2023-02-23 03:46:51 +0530

    use "WebClient" to communicate with the service2

--- keycloak-client-credentials-demo-service1/src/main/java/com/learn/keycloak/controller/Controller1.java
+++ keycloak-client-credentials-demo-service1/src/main/java/com/learn/keycloak/controller/Controller1.java
@@ -11,24 +11,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/microservice1")
 public class Controller1 {
 
+	private static final String MICROSERVICE_2_URL = "http://localhost:8084/microservice2/home";
+
 	private final RestTemplate restTemplate = new RestTemplateBuilder().build();
+	private final WebClient webClient = WebClient.builder().build();
 
 	@GetMapping("/home")
-	public String hello() {
+	public String helloRestClient() {
 		Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "Bearer " + jwt.getTokenValue());
 		ResponseEntity<String> response = restTemplate.exchange(
-				"http://localhost:8084/microservice2/home",
+				MICROSERVICE_2_URL,
 				HttpMethod.GET,
 				new HttpEntity<>(headers),
 				String.class);
 
 		return "hello from microservice 1 and " + response.getBody() ;
+	}
+
+	@GetMapping("/home/web-client")
+	public String helloWebClient() {
+		Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		String response = webClient
+				.get()
+				.uri(MICROSERVICE_2_URL)
+				.headers(header -> header.setBearerAuth(jwt.getTokenValue()))
+				.retrieve()
+					.bodyToMono(String.class)
+					.block();
+
+		return "hello from microservice 1 and " + response ;
 	}
 }
