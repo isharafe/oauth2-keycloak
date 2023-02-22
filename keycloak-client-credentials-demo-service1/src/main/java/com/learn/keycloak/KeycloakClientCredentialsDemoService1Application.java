e2b8723ac359fe457069187c49e7b9955e2544c6
Ishara <isharafe@gmail.com>
2023-02-23 02:49:53 +0530

    initial spring boot appliacation. This application is supposed to act as both client and a resource server. So created with dependencies, web-starter, spring-security, oauth2-client, oauth2-resource-server, oauth2-jose

---
+++ keycloak-client-credentials-demo-service1/src/main/java/com/learn/keycloak/KeycloakClientCredentialsDemoService1Application.java
@@ -0,0 +1,13 @@
+package com.learn.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakClientCredentialsDemoService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakClientCredentialsDemoService1Application.class, args);
+	}
+
+}
