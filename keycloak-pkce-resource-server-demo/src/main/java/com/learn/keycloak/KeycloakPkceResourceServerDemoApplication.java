39fb23e66d08d32aeb1e5ca1486774b095b0782f
Ishara <isharafe@gmail.com>
2023-02-23 00:52:54 +0530

    pkce flow - resource_server - initial spring boot project for the resource server. using dependencies, web-starter, security-starter, oauth2-resource-server, oauth2-jose

---
+++ keycloak-pkce-resource-server-demo/src/main/java/com/learn/keycloak/KeycloakPkceResourceServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakPkceResourceServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakPkceResourceServerDemoApplication.class, args);
+	}
+
+}
---
+++ keycloak-pkce-resource-server-demo/src/test/java/com/learn/keycloak/KeycloakPkceResourceServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakPkceResourceServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
